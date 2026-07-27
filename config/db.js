d801097ffd2b6c49b7c5cb7592501e6d965caf73
Rayhan Ali Firmansyah <rayhanalifirmanzyah88@gmail.com>
2026-07-28 01:02:25 +0700

    Debug Railway environment

--- config/db.js
+++ config/db.js
@@ -1,4 +1,17 @@
+console.log("========== ENV ==========");
+console.log("MYSQLHOST:", process.env.MYSQLHOST);
+console.log("MYSQLPORT:", process.env.MYSQLPORT);
+console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE);
+console.log("MYSQLUSER:", process.env.MYSQLUSER);
+console.log("MYSQLPASSWORD:", process.env.MYSQLPASSWORD ? "******" : "undefined");
+console.log("=========================");
+
 const mysql = require("mysql2");
+
+console.log("MYSQLHOST =", process.env.MYSQLHOST);
+console.log("MYSQLUSER =", process.env.MYSQLUSER);
+console.log("MYSQLDATABASE =", process.env.MYSQLDATABASE);
+console.log("MYSQLPORT =", process.env.MYSQLPORT);
 
 const db = mysql.createConnection({
     host: process.env.MYSQLHOST || "localhost",
